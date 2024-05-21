4b4397d88408abf4ce20c27925dff2cf23666a53
Mike <mikebio@gmail.com>
2024-05-21 13:01:55 +0000

    Assignment 3 MVC

---
+++ app/controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home extends Controller {
+
+    public function index() {
+      $user = $this->model('User');
+      $data = $user->test();
+
+			// NOTE: if you just cloned the repo, after you update your DB_PASS, you will land here. You can do an echo 123; die; and see that you are here! But we are instead going to the home/index file.
+			
+	    $this->view('home/index');
+	    die;
+    }
+
+}
