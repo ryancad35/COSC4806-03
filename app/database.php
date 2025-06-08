5e51ab922e2990ee9adc5dd2889e26af51a09389
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-06-08 22:31:57 +0000

    Connection to DB made; deleted index.php from root folder; getting app to serve the index.php from /public folder instead.

--- app/database.php
+++ app/database.php
@@ -4,9 +4,6 @@
  * 
  */
 
-
-// function db_query($query);
-
 function db_connect() {
     try { 
         $dbh = new PDO('mysql:host=' . DB_HOST . ';port='. DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
