d0601c7a0166f2c31a06ef0b4bd4a6e7adc2bae9
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-06-08 20:56:00 +0000

    Test commit

--- app/database.php
+++ app/database.php
@@ -4,6 +4,9 @@
  * 
  */
 
+
+// function db_query($query);
+
 function db_connect() {
     try { 
         $dbh = new PDO('mysql:host=' . DB_HOST . ';port='. DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
